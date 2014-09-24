594441a8f845417ef210b0fcddf2c37dc23af107
Oleksandr Zinenko <zinenko@lri.fr>
2014-09-24 14:01:04 +0200

    Enumerator of points in the union of polyhedra

---
+++ enumerator.cpp
@@ -0,0 +1,71 @@
+#include "enumerator.h"
+
+/// Application-wide ISL context
+ISLContextRAII ISLEnumerator::islContext_;
+
+namespace {
+int addISLPointToVector(isl_point *point, void *vect) {
+  CLINT_ASSERT(point != nullptr, "Point is nullptr");
+  CLINT_ASSERT(vect != nullptr, "Vector is nullptr");
+
+  std::vector<std::vector<int> > &vector = *static_cast<std::vector<std::vector<int> > *>(vect);
+  isl_space *space = isl_point_get_dim(point);
+  const unsigned nbDims = isl_dim_size(space, isl_dim_all);
+  std::vector<int> pointVector;
+  pointVector.reserve(nbDims);
+  for (unsigned i = 0; i < nbDims; i++) {
+    isl_val *val = isl_point_get_coordinate_val(point, isl_dim_all, i);
+    long value = isl_val_get_num_si(val);
+    CLINT_ASSERT(isl_val_get_den_si(val) == 1, "Fractional point");
+    CLINT_ASSERT(value <= INT_MAX, "Integer overflow");
+    int ivalue = static_cast<int>(value);
+    pointVector.push_back(ivalue);
+    isl_val_free(val);
+  }
+  isl_space_free(space);
+  vector.push_back(std::move(pointVector));
+
+  return 0;
+}
+} // end anonymous namespace
+
+std::vector<std::vector<int> > ISLEnumerator::enumerate(osl_relation_p relation, const std::vector<int> &dimensions) {
+  isl_set *set = setFromOSLRelation(relation);
+
+  std::vector<std::vector<int> > points;
+  std::vector<int> allDimensions, dimensionsToProjectOut;
+  // Fill non-local dimension indices.
+  allDimensions.reserve(relation->nb_columns - 2);
+  for (int i = 0; i < relation->nb_columns - 2; i++) {
+    if (i < relation->nb_input_dims + relation->nb_output_dims ||
+        i > relation->nb_input_dims + relation->nb_output_dims + relation->nb_local_dims) {
+      allDimensions.push_back(i);
+    }
+  }
+  std::set_difference(std::begin(allDimensions),
+                      std::end(allDimensions),
+                      std::begin(dimensions),
+                      std::end(dimensions),
+                      std::begin(dimensionsToProjectOut));
+
+  for (int dim : dimensionsToProjectOut) {
+    isl_dim_type dim_type;
+    int index;
+    std::tuple<isl_dim_type, int> tuple = ISLEnumerator::dimFromOSL(relation, dim);
+    std::tie(dim_type, index) = tuple;
+    set = isl_set_project_out(set, dim_type, index, 1);
+  }
+  isl_set_foreach_point(set, &addISLPointToVector, &points);
+  isl_set_free(set);
+  return std::move(points); // Force RVO.
+}
+
+std::vector<int> ISLEnumerator::map(osl_statement_p stmt, const std::vector<int> &point) {
+  (void) stmt;
+  (void) point;
+  CLINT_ASSERT(false, "Unimplemented");
+  return std::move(std::vector<int>());
+}
+
+ISLEnumerator::~ISLEnumerator() {
+}
---
+++ enumerator.h
@@ -0,0 +1,143 @@
+#ifndef ENUMERATOR_H
+#define ENUMERATOR_H
+
+#include <isl/set.h>
+#include <isl/map.h>
+#include <isl/ctx.h>
+
+#include <osl/osl.h>
+
+#include "macros.h"
+
+class Enumerator {
+public:
+  /**
+   * @brief Get a list of integer points in the polytope, projected to a set of specific dimensions.
+   * @param [in] relation   Union of relations that defines a polytope.
+   * @param [in] dimensions Vector of indices of dimensions to project points onto.
+   * @return The vector of integer points inside the polyhedron, each point represented as a vector of integer coordinates ordered
+   * by the dimension index.
+   */
+  virtual std::vector<std::vector<int>> enumerate(osl_relation_p relation, const std::vector<int> &dimensions) = 0;
+  /**
+   * @brief Get the logical execution date of a statement instance identified by the given vector if iterator values.
+   * @param [in] stmt  Statement, instance of which is being projected.
+   * @param [in] point Iterator values for the statement instance.
+   * @return Logical execution date in beta-alpha format.
+   */
+  virtual std::vector<int> map(osl_statement_p stmt, const std::vector<int> &point) = 0;
+  /**
+   * @brief Virtual desctructor.  Reimplement in all derived classes with non-trival memory management.
+   */
+  virtual ~Enumerator() {}
+};
+
+class ISLContextRAII {
+public:
+  ISLContextRAII() {
+  }
+
+  ~ISLContextRAII() {
+    isl_ctx_free(ctx_);
+  }
+
+  isl_ctx *ctx() {
+    if (ctx_ == nullptr) {
+      ctx_ = isl_ctx_alloc();
+    }
+    return ctx_;
+  }
+
+  isl_ctx *operator ()() {
+    return ctx();
+  }
+
+  operator isl_ctx *() {
+    return ctx();
+  }
+
+private:
+  isl_ctx *ctx_;
+};
+
+/**
+ * @brief An implementation of Enumerator using ISL library.
+ */
+class ISLEnumerator : public Enumerator {
+public:
+  std::vector<std::vector<int> > enumerate(osl_relation_p relation, const std::vector<int> &dimensions) override;
+  std::vector<int> map(osl_statement_p stmt, const std::vector<int> &point) override;
+
+  ~ISLEnumerator() override;
+
+  static std::tuple<isl_dim_type, int> dimFromOSL(osl_relation_p relation, int dimension) {
+    CLINT_ASSERT(dimension >= 0, "Negative dimension");
+    CLINT_ASSERT(dimension < relation->nb_columns - 2, "Dimension overflow");
+    if (dimension < relation->nb_input_dims)
+      return std::make_tuple(isl_dim_in, dimension);
+    dimension -= relation->nb_input_dims;
+    if (dimension < relation->nb_output_dims)
+      return std::make_tuple(isl_dim_out, dimension);
+    dimension -= relation->nb_output_dims;
+    if (dimension < relation->nb_local_dims)
+      CLINT_ASSERT(false, "Local dimensions are not directly accessible in ISL");
+    dimension -= relation->nb_local_dims;
+    if (dimension < relation->nb_parameters)
+      return std::make_tuple(isl_dim_param, dimension);
+
+    CLINT_UNREACHABLE;
+  }
+
+  static inline __isl_give isl_set *setFromOSLRelation(osl_relation_p relation, osl_names_p names = nullptr) {
+    return osl2isl(isl_set_read_from_str, relation, names);
+  }
+
+  static inline __isl_give isl_map *mapFromOSLRelation(osl_relation_p relation, osl_names_p names = nullptr) {
+    return osl2isl(isl_map_read_from_str, relation, names);
+  }
+
+  static inline osl_relation_p setToOSLRelation(isl_set *set) {
+    return isl2osl(isl_printer_print_set, set);
+  }
+
+  static inline osl_relation_p basicSetToOSLRelation(isl_basic_set *basicSet) {
+    return isl2osl(isl_printer_print_basic_set, basicSet);
+  }
+
+  static inline osl_relation_p mapToOSLRelation(isl_map *map) {
+    return isl2osl(isl_printer_print_map, map);
+  }
+
+private:
+  static ISLContextRAII islContext_;
+
+  template <typename T>
+  static osl_relation_p isl2osl(isl_printer *(&Func)(isl_printer *, T *), T *t) {
+    CLINT_ASSERT(t != nullptr, "ISL object is null")
+
+    isl_printer *prn = isl_printer_to_str(islContext_);
+    prn = isl_printer_set_output_format(prn, ISL_FORMAT_EXT_POLYLIB);
+    prn = Func(prn, t);
+    char *str = isl_printer_get_str(prn);
+    char *oslStr = new char[strlen(str) + 11];
+    char *oslStrKeeper = oslStr; // osl_relation_sread advances the pointer...
+    sprintf(oslStr, "UNDEFINED\n%s", str);
+    osl_relation_p relation = osl_relation_sread(&oslStr); // TODO: add osl_relation_sread_polylib to facilitate this
+    delete [] oslStrKeeper;
+    free(str);
+    isl_printer_free(prn);
+    return relation;
+  }
+
+  template <typename T>
+  static T *osl2isl(T *(&Func)(isl_ctx *, const char *), osl_relation_p relation, osl_names_p names = nullptr) {
+    CLINT_ASSERT(relation != nullptr, "Relation pointer is null");
+
+    char *string = osl_relation_spprint_polylib(relation, NULL);
+    T *t = Func(islContext_, string);
+    free(string);
+    return t;
+  }
+};
+
+#endif // ENUMERATOR_H
