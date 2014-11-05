ef6924cecfa8995b3c798c6deb9416db315dc7d7
Oleksandr Zinenko <zinenko@lri.fr>
2014-11-05 14:41:29 +0100

    Draw proper arrows

--- vizdeparrow.cpp
+++ vizdeparrow.cpp
@@ -19,16 +19,23 @@ void VizDepArrow::paint(QPainter *painter, const QStyleOptionGraphicsItem *optio
   Q_UNUSED(widget);
 
   painter->drawLine(m_arrowLine);
-  painter->drawPath(m_arrowHead);
+  painter->fillPath(m_arrowHead, QBrush(Qt::black));
 }
 
 QRectF VizDepArrow::boundingRect() const {
-  // FIXME: calculate properly and only once
-  return QRectF(m_arrowLine.p1(), m_arrowLine.p2()) |
-         m_arrowHead.boundingRect();
+  // Since arrow head has a 60-degree angle, it will be covered by a rectangle
+  // (with a 90-degree angle, obviously) ending at the head's sharp end.  This sharp end
+  // lies on the arrow line, pointRadius pixels after the arrow line ends.
+  const double pointRadius =
+      m_polyhedron->coordinateSystem()->projection()->vizProperties()->pointRadius();
+  QLineF diagonal(m_arrowHead);
+  diagonal.setLength(diagonal.length() + pointRadius);
+  return QRectF(m_arrowLine.p1(), m_arrowLine.p2());
 }
 
 void VizDepArrow::pointLink(QPointF source, QPointF target) {
+  // Set the line so that it starts on the border of the first point's circle, and ends pointRadius
+  // pixels before the second point's circle in order to put the arrow head there.
   const double pointRadius =
       m_polyhedron->coordinateSystem()->projection()->vizProperties()->pointRadius();
   m_arrowLine = QLineF(source, target);
@@ -39,6 +46,25 @@ void VizDepArrow::pointLink(QPointF source, QPointF target) {
   m_arrowLine.setLength(length - 2. * pointRadius);
   m_arrowLine.setP1(displacementLine.p2());
 
-  // TODO: draw a proper arrow here
-  m_arrowHead.addEllipse(m_arrowLine.p2(), 0.3 * pointRadius, 0.3 * pointRadius);
+  // Compute an arrow path of the style )>.
+  // Head is based on the triangle with equal sides and an arc.
+  // Center of the arc touches the arrow line, side angles are 1/4 R back along this line.
+  // Hence triangle height is H = R + 1/4 R.
+  // Thus length of the triangle side is, L = 5 / 2 / sqrt(3) R,
+  // where R is point readius.
+  const double halfside = 5.0 * pointRadius / 4.0 / sqrt(3.0);
+  QPainterPath headPath;
+  headPath.moveTo(-halfside, pointRadius / 4.0);
+  headPath.lineTo(0, -pointRadius);
+  headPath.lineTo(halfside, pointRadius / 4.0);
+  const double angle = acos(halfside / pointRadius);
+  headPath.arcTo(-pointRadius, 0, 2.0 * pointRadius, 2.0 * pointRadius,
+                 90 - (angle * 180) / M_PI,
+                 2 * (angle * 180) / M_PI);
+  QTransform transform;
+  transform.rotate(m_arrowLine.angle() + 180);
+  headPath = transform.map(headPath);
+  transform.reset();
+  transform.translate(m_arrowLine.p2().x(), m_arrowLine.p2().y());
+  m_arrowHead = transform.map(headPath);
 }
